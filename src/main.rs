bb4a9b74110c74d0fba07a714fdc58c87b2dab87
HuuLoc <huuloc2026@gmail.com>
2025-08-10 21:58:36 +0700

    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello, world! This is a simple Actix-web server. From Jake Onyx")
+}
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("🚀 Server đang chạy tại http://127.0.0.1:8080");
+
+
+    HttpServer::new(|| {
+        App::new().service(hello)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
